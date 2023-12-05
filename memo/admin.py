07f8e42468de4d25b3464d312d2a3b577fd93285
Yao Jean Dotsey <yaojeandotsey1234@gmail.com>
2023-12-05 20:49:51 +0000

    creation des modèle Utilisateur, Enseignant, Etudiant

--- memo/admin.py
+++ memo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Utilisateur, Etudiant, Enseignant
 
 # Register your models here.
+
+
+admin.site.register(Utilisateur)
+admin.site.register(Etudiant)
+admin.site.register(Enseignant)
